3b3a9729fc221e9ce685ff1557557e971bedf09c
paolamelch <p.melchiorrericci@gmail.com>
2023-05-24 14:14:29 +0200

    esercizio 1

---
+++ my-app1/src/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Hello extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>It's time to learn React!</h1>
+            </div>
+        )
+    }
+}
+
