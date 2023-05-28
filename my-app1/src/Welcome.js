c3fc4f7d2353ab027a0e67a2af097c1583746772
paolamelch <p.melchiorrericci@gmail.com>
2023-05-28 15:15:31 +0200

    esercizio 8

--- my-app1/src/Welcome.js
+++ my-app1/src/Welcome.js
@@ -7,7 +7,7 @@ export class Welcome extends React.Component{
         return(
             <div>
                 <p>Welcome,<strong>{this.props.name}</strong>!</p>
-                <p>{this.props.age > 18 && <Age age ={this.props.age}/>}</p>
+                <p>{this.props.age && <Age age ={this.props.age}/>}</p>
             </div>
         )
     }
