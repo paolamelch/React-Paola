8dffd75fb78cf6dc705ddead1a6019388d3f45e7
paolamelch <p.melchiorrericci@gmail.com>
2023-05-31 15:03:02 +0200

    esercizio 17

--- my-app1/src/App.js
+++ my-app1/src/App.js
@@ -3,6 +3,8 @@ import { Hello } from "./Hello";
 import { Welcome } from "./Welcome";
 import { Counter } from "./Counter";
 import { ClickCounter } from "./ClickCounter";
+import { ClickTracker } from "./ClickTracker";
+
 
 
 export class App extends React.Component{
@@ -15,7 +17,7 @@ export class App extends React.Component{
                 name = {name}
                 age ={25}
                 />
-                <ClickCounter/>
+                <ClickTracker/>
             </>
         )
     }
