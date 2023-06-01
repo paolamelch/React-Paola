875a4bc501124ff5665e3df1d55ac559bda1867d
paolamelch <p.melchiorrericci@gmail.com>
2023-06-01 12:17:36 +0200

    esercizio 21

---
+++ my-app1/src/UncontrolledLogin.js
@@ -0,0 +1,30 @@
+import { createRef } from "react";
+import React from "react";
+
+export class UncontrolledLogin extends React.Component{
+    _formRef = createRef();
+    handleSubmitForm = (event) =>{
+
+        event.preventDefault();
+
+        const username = event.target.elements.username.value;
+        const password = event.target.elements.password.value;
+        const remember = event.target.elements.remember.checked;
+
+
+    }
+
+    render(){
+        return(
+            <div>
+                <form ref={this._formRef} onSubmit={this.handleSubmitForm}>
+                    <input name="username"/>
+                    <input name="password" type="password"/>
+                    <input name="remember" type="checkbox"/>
+                    <button type="submit">Login</button>
+                    <button type="reset">Reset</button>
+                </form>
+            </div>
+        )
+    }
+}
