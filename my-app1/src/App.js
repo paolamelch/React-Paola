6c698e2027e3fdfed87f6a496c3ca668b53ceea4
paolamelch <p.melchiorrericci@gmail.com>
2023-06-01 13:12:38 +0200

    esercizio 23

--- my-app1/src/App.js
+++ my-app1/src/App.js
@@ -7,6 +7,7 @@ import { ClickTracker } from "./ClickTracker";
 import { InteractiveWelcome } from "./InteractiveWelcome";
 import { Login } from "./Login";
 import { UncontrolledLogin } from "./UncontrolledLogin";
+import { ToDoList } from "./ToDoList";
 
 
 
@@ -16,7 +17,7 @@ export class App extends React.Component{
         return(
             <>
                 <Hello />
-                <UncontrolledLogin/>
+                <ToDoList/>
                 <ClickTracker/>
             </>
         )
