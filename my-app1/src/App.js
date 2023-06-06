d138595c44b793e34d6e3e9c2045ed313a5d6f03
paolamelch <p.melchiorrericci@gmail.com>
2023-06-06 10:37:41 +0200

    esercizio 28

--- my-app1/src/App.js
+++ my-app1/src/App.js
@@ -8,16 +8,36 @@ import { InteractiveWelcome } from "./InteractiveWelcome";
 import { Login } from "./Login";
 import { UncontrolledLogin } from "./UncontrolledLogin";
 import { ToDoList } from "./ToDoList";
+import { DisplayContext, LanguageContext } from "./DisplayContext";
+
 
 
 
 export class App extends React.Component{
+
+    state = {
+        language:"en",
+    }
+
+    handleLanguageChange = (event) =>{
+        this.setState({language: event.target.value})
+    }
+
+    componentDidUpdate(){
+        console.log(this.state);
+    }
     render(){
         const name = "John";
         return(
             <>
+            <select value={this.state.language} onChange={this.handleLanguageChange}>
+                <option value="en">English</option>
+                <option value="it">Italiano</option>
+            </select>
+            <LanguageContext.Provider value={this.state.language}>
+                <DisplayContext/>
+            </LanguageContext.Provider>
                 <Hello />
-                <ToDoList />
                 <ClickTracker/>
             </>
         )
