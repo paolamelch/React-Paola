e7041669d405c85b3b77a21f98fe5501b4e41e19
paolamelch <p.melchiorrericci@gmail.com>
2023-06-14 22:05:24 +0200

    esercizio 39

--- my-app1/src/UseGitHubUser.js
+++ my-app1/src/UseGitHubUser.js
@@ -2,13 +2,28 @@ import { useState,useEffect } from "react"
 
 export function useGithubUser(username){
     const [data, setData] = useState('')
+    const [loading, setLoading]= useState(false)
+    const [error,setError] = useState(null)
 
     useEffect(() => {
+      setLoading(true)
+      setError(null)
         fetch(`https://api.github.com/users/${username}`)
-        .then((response)=> response.json())
-        .then((data)=> setData(data.name))
-      }, []);
+        .then((response)=> {
+          if (response.status !== 200){
+            setError(new Error('User not found'))
+          }
 
-    return{data}
+          return response.json()
+        })
+        .then((json) =>{
+          setData(json.name)
+        })
+        .catch((error) =>{
+          setError(error)
+        })
+        },[username])
+
+    return{data,loading,error}
 
 }
