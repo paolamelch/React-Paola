e5f806bb46367848cc1fc59a364b874c6d20344d
paolamelch <p.melchiorrericci@gmail.com>
2023-06-15 15:17:30 +0200

    esercizio 48

--- my-app1/src/ShowGithubUser.js
+++ my-app1/src/ShowGithubUser.js
@@ -1,10 +1,13 @@
-import { useParams } from "react-router-dom"
+import { Outlet, useParams } from "react-router-dom"
 import { GithubUser } from "./GithubUser"
 
 export function ShowGithubUser(){
     const {username = "paolamelch"} = useParams()
 
     return(
+        <>
         <GithubUser username={username}/>
+        </>
+        
     )
 }
