8d335605bb4fbe3ed83c99e8484cff940ad2b7f6
paolamelch <p.melchiorrericci@gmail.com>
2023-06-15 11:48:02 +0200

    esericizio 41

--- my-app1/src/App.js
+++ my-app1/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {Hello} from "./Hello";
 import {Login} from "./Login";
 import {UncontrolledLogin} from "./UncontrolledLogin";
@@ -8,20 +8,32 @@ import { GitHubUserList } from "./GithubUserList";
 import { ClickCounter } from "./ClickCounter";
 import { GithubUser } from "./GithubUser";
 import { CarDetails } from "./CarDetails";
+import { DisplayContext, LanguageContext } from "./DisplayContext";
 
 
 function App(props) {
 
-    const handleGitHubUserChange = (username) => {
-        return(
-            <div>{username.login}</div>
-        )
+    // const handleGitHubUserChange = (username) => {
+    //     return(
+    //         <div>{username.login}</div>
+    //     )
+    // }
+
+    const [language,setLanguage] = useState('en')
+
+    function handleChangeSelect(event){
+        setLanguage(event.target.value)
     }
-
-
     return (<>
+    <select onChange={handleChangeSelect}>
+        <option value="en">English</option>
+        <option value="it">Italiano</option>
+    </select>
+    <LanguageContext.Provider value={language}>
         <Hello/>
-        <CarDetails initialData={props}/>
+        <DisplayContext/>
+    </LanguageContext.Provider>
+
     </>)
 }
 export default App
